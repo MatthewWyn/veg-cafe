ecc0771287127c5e7d8d23bfae265470cfb26547
Matt <matthewdavies88@hotmail.com>
2018-02-14 21:21:16 +0000

    Flexbox complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,2 @@
-const gulp   = require('gulp');
-const watch  = require('gulp-watch');
-
-gulp.task('default', function() {
-	console.log("YAYY!!!!! GULP TASK C R E A T E D !!!!!!")
-});
-
-
-gulp.task('html', function() {
-	console.log("Imagine the HTML here!");
-
-});
-
-
-gulp.task('watch', function() {
-	
-	watch('./app/index.html', function() {
-		gulp.start('html');
-	});
-
-});
-
+require ('./gulp/tasks/styles');
+require ('./gulp/tasks/watch');
